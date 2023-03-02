d246025f4fb230a8fdc25c86abfada2c9a294622
Kay Steinhoff <kay.steinhoff@web.de>
2023-03-02 13:02:22 +0100

    Update ClientTest.c

--- examples/ClientTest.c
+++ examples/ClientTest.c
@@ -5,7 +5,7 @@ int main(int argc, char const* argv[])
 	struct Client myClient;
 	myClient.ip_address = "127.0.0.1";
 	myClient.port = 8000;
-	if(SetupClient(&myClient) < 0)
+	if(SetupClient(&myClient) != 0)
 		printf("Cloudn't connect!");
 	printf("Created Server!\n");
 	
